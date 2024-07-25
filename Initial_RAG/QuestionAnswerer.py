cde67ab239338c2c2267a8427a691e8234cbb002
Martin Fixman <martinfixman@gmail.com>
2024-07-25 13:42:50 +0100

    Adding falcon version.

---
+++ Initial_RAG/QuestionAnswerer.py
@@ -0,0 +1,68 @@
+import warnings
+warnings.simplefilter(action = 'ignore', category = FutureWarning)
+
+from transformers import *
+import logging
+
+Model_dict = {
+    'falcon2': 'tiiuae/falcon-11b',
+    'falcon-180b': 'tiiuae/falcon-180b-chat',
+    'falcon-40b': 'tiiuae/falcon-40b-instruct',
+    'falcon-7b': 'tiiuae/falcon-7b-instruct',
+}
+
+class Model:
+    def __init__(self, name):
+        self.name = name
+        self.model_name = Model_dict[name]
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = self.getModel(self.model_name)
+
+    def getModel(self, model_name):
+        logging.info(f'Getting {model_name}')
+        try:
+            return AutoModelForCausalLM.from_pretrained(model_name)
+        except OSError:
+            pass
+
+        for a in range(2, 10):
+            logging.info(f'Attempt {a}/10 for {model_name}')
+            try:
+                return AutoModelForCausalLM.from_pretrained(
+                    model_name,
+                    force_download = True,
+                    resume_download = False,
+                )
+            except OSError:
+                pass
+
+        logging.error('Failed 10 attempts for {model_name}. Giving up.')
+        raise
+
+class QuestionAnswerer:
+    def __init__(self, model_names, device = 'cpu'):
+        self.device = device
+        
+        assert all(x in Model_dict for x in model_names)
+        self.llms = [Model(x) for x in model_names]
+
+    def query(self, question):
+        answers = {}
+        for llm in self.llms:
+            logging.info('Tokenising')
+            inputs = llm.tokenizer(question, return_tensors = "pt", truncation = True)
+
+            logging.info('Generating')
+            outputs = llm.model.to(self.device).generate(
+                inputs["input_ids"],
+                max_length = 300,
+                num_return_sequences = 1,
+                pad_token_id = llm.tokenizer.eos_token_id,
+            )
+
+            logging.info('Decoding')
+            answer = llm.tokenizer.decode(outputs[0], skip_special_tokens = True)
+
+            answers[llm.name] = answer
+
+        return answers
---
+++ Initial_RAG/models.py
@@ -0,0 +1,45 @@
+import warnings
+warnings.simplefilter(action = 'ignore', category = FutureWarning)
+
+from argparse import ArgumentParser
+from transformers import *
+import logging
+
+from QuestionAnswerer import QuestionAnswerer, Model_dict
+
+def parse_args():
+    parser = ArgumentParser(description = 'Ask me a question')
+    parser.add_argument(
+        '-m',
+        '--models',
+        '--model-list',
+        '--model',
+        type = str.lower,
+        choices = Model_dict.keys(),
+        default = ['falcon-7b'],
+        nargs = '+',
+        help = 'Which model or models to use',
+    )
+    parser.add_argument('--device', choices = ['cpu', 'cuda'], default = 'cpu')
+
+    parser.add_argument('questions', nargs = '*', default = 'Where was Obama born?')
+    return parser.parse_args()
+
+def main():
+    logging.getLogger('transformers').setLevel(logging.ERROR)
+    logging.basicConfig(
+        format='[%(asctime)s] %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    args = parse_args()
+    answerer = QuestionAnswerer(args.models, device = args.device)
+
+    for q in args.questions:
+        answers = answerer.query(q)
+        for llm, answer in answers.items():
+            print(f'\033[1m{llm}\033[0m: {answer}')
+
+if __name__ == '__main__':
+    main()
