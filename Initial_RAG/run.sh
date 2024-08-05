a6d74eb7be107c19b51e8d5ea7db7c8c81edc167
Martin Fixman <martin.fixman@city.ac.uk>
2024-08-05 16:45:43 +0100

    Adding SLURM files.

---
+++ Initial_RAG/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name ragdummy
+#SBATCH --partition=prigpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=72GB
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -e results/%x_%j.e
+#SBATCH -o results/%x_%j.o
+#SBATCH --error run.err
+#SBATCH --output run.out
+
+source /opt/flight/etc/setup.sh
+flight env activate gridware
+module load libs/nvidia-cuda/11.2.0/bin
+module load gnu
+module add compilers/gcc gnu
+
+export http_proxy=http://hpc-proxy00.city.ac.uk:3128
+export https_proxy=http://hpc-proxy00.city.ac.uk:3128
+export HTTP_PROXY=http://hpc-proxy00.city.ac.uk:3128
+export HTTPS_PROXY=http://hpc-proxy00.city.ac.uk:3128
+
+export WANDB_API_KEY=9692ff12f6990a08e1a75d22ddd651d0f3de3e95
+export TORCH_HOME=/mnt/data/public/torch
+
+job=$1
+shift
+
+python ${job} "$@"
